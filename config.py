ac18c4ed39fa4c62dd7a6b1ad69d01eabc5aadf0
Руслан Косенков <verlincon@yandex.ru>
2025-11-14 23:36:36 +0300

    файл конфигурации для подключения к БД
     у меня нестандартный пароль в postgresSQL ("12345678")

---
+++ config.py
@@ -0,0 +1,6 @@
+# Параметры подключения к базе данных
+DB_HOST = "localhost"
+DB_PORT = 5432
+DB_USER = "postgres"
+DB_PASSWORD = "12345678" #у меня нестандартный пароль в postgresSQL
+DB_NAME = "products_db"
