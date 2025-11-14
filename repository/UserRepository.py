bdcaec4b2ccc1f6562ce867f3097bc7fbcdc8f2e
ekaterinakiseleva21 <autumnmurka@gmail.com>
2025-11-14 21:31:43 +0300

    Update UserRepository.py

--- repository/UserRepository.py
+++ repository/UserRepository.py
@@ -1,7 +1,5 @@
 from psycopg2.extras import RealDictCursor
-
 from db.DatabaseConnection import DatabaseConnection
-
 
 class UserRepository:
     def __init__(self):
@@ -10,4 +8,12 @@ class UserRepository:
     def get_user_by_username(self, username: str):
         with self.db.get_connection() as conn, conn.cursor(cursor_factory=RealDictCursor) as cur:
             cur.execute("SELECT * FROM users WHERE username = %s", (username,))
+            return cur.fetchone()
+
+    # ДОБАВЬ ЭТОТ МЕТОД
+    def create_user(self, username: str, password: str):
+        with self.db.get_connection() as conn, conn.cursor(cursor_factory=RealDictCursor) as cur:
+            cur.execute("INSERT INTO users (username, password) VALUES (%s, %s) RETURNING *;", 
+                       (username, password))
+            conn.commit()
             return cur.fetchone()
