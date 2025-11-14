30c0d079a613a0ebcf6464cd806e09daf0759ae8
Nefqu <149079598+Nefqu@users.noreply.github.com>
2025-11-15 00:18:17 +0300

    Update DatabaseConnection.py

--- db/DatabaseConnection.py
+++ db/DatabaseConnection.py
@@ -1,12 +1,13 @@
 import psycopg2
+from db.config import db_host, db_port, db_name, db_user, db_password
 
 class DatabaseConnection:
     def __init__(self):
-        self.host = "localhost"
-        self.port = 5432
-        self.user = "postgres"
-        self.password = "postgres"
-        self.dbname = "products_db"
+        self.host = db_host
+        self.port = db_port
+        self.user = db_user
+        self.password = db_password
+        self.dbname = db_name
 
     def get_connection(self):
         return psycopg2.connect(
