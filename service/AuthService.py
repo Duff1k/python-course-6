d16e112f6f1101d1d2e1fff9593f6054fe42d65f
ekaterinakiseleva21 <autumnmurka@gmail.com>
2025-11-14 21:30:51 +0300

    Update AuthService.py

--- service/AuthService.py
+++ service/AuthService.py
@@ -1,12 +1,17 @@
+import hashlib
 from repository.UserRepository import UserRepository
-
 
 class AuthService:
     def __init__(self):
         self.user_repo = UserRepository()
 
+    
+    def hash_password(self, password: str) -> str:
+        return hashlib.sha256(password.encode()).hexdigest()
+
     def verify_password(self, username: str, password: str) -> bool:
         user = self.user_repo.get_user_by_username(username)
         if not user:
             return False
-        return user["password"] == password
+        
+        return user["password"] == self.hash_password(password)
