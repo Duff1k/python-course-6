134650fdd7dcf569658b3ee7f7fd112765e0892a
malkovaanya <nusha.04@bk.ru>
2025-11-14 18:53:44 +0300

    add config for bd

---
+++ config.py
@@ -0,0 +1,6 @@
+# Параметры подключения к базе данных
+DB_HOST = "localhost"
+DB_PORT = 5432
+DB_USER = "postgres"
+DB_PASSWORD = "postgres"
+DB_NAME = "products_db"
--- db/DatabaseConnection.py
+++ db/DatabaseConnection.py
@@ -1,12 +1,14 @@
 import psycopg2
+from config import DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME
+
 
 class DatabaseConnection:
     def __init__(self):
-        self.host = "localhost"
-        self.port = 5432
-        self.user = "postgres"
-        self.password = "postgres"
-        self.dbname = "products_db"
+        self.host = DB_HOST
+        self.port = DB_PORT
+        self.user = DB_USER
+        self.password = DB_PASSWORD
+        self.dbname = DB_NAME
 
     def get_connection(self):
         return psycopg2.connect(
@@ -14,5 +16,5 @@ class DatabaseConnection:
             port=self.port,
             user=self.user,
             password=self.password,
-            dbname = self.dbname
+            dbname=self.dbname
         )
