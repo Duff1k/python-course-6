602d6aa232dca10513e1d69ba2448cd118d181ac
Nefqu <149079598+Nefqu@users.noreply.github.com>
2025-11-15 00:19:29 +0300

    Update AuthService.py

--- service/AuthService.py
+++ service/AuthService.py
@@ -1,3 +1,4 @@
+import hashlib
 from repository.UserRepository import UserRepository
 
 
@@ -6,7 +7,9 @@ class AuthService:
         self.user_repo = UserRepository()
 
     def verify_password(self, username: str, password: str) -> bool:
+        print("USERNAME =", username)
+        print("PASSWORD =", password)
         user = self.user_repo.get_user_by_username(username)
         if not user:
             return False
-        return user["password"] == password
+        return user["password"] == hashlib.sha256(password.encode()).hexdigest()
